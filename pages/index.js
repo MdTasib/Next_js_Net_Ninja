22833026332fc12b2807d482f687badeb416dbfe
MdTasib <ohidulalam442@gmail.com>
2021-07-07 12:13:21 +0600

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import Navbar from "../components/Navbar";
-import Footer from '../components/Footer';
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h2>Home Page</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium expedita distinctio inventore sit explicabo pariatur, illo aliquam repellendus nulla ad.</p>
-      <Footer />
     </div>
   )
 }
